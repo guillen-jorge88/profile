6ab8e181b626c907b629bb805979229806560ff7
guillen-jorge88 <guillen.jorge88@gmail.com>
2020-04-14 12:32:08 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prof-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'profile';
+}
